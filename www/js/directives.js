849d7b018e012318245585c31b1058f275b4eca6
julbarrientos <julbarrientos@gmail.com>
2017-02-25 01:00:11 -0300

    fix input monto

--- www/js/directives.js
+++ www/js/directives.js
@@ -1,5 +1,21 @@
 angular.module('app.directives', [])
-
+.directive('noFloat', function () {
+return  {
+    restrict: 'A',
+    link: function (scope, elm, attrs, ctrl) {
+        elm.on('keydown', function (event) {
+          if ([110, 190].indexOf(event.which) > -1) {
+                // dot and numpad dot
+                event.preventDefault();
+                return false;
+            }
+            else{
+              return true;
+            }
+        });
+    }
+}
+})
 .directive('qrcode', function($interpolate) {  
   return {
     restrict: 'E',
